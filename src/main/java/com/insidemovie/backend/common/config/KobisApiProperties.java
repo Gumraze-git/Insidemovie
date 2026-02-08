85f8ae7c3d3c6f64bdf1fde2c0446e4baf89b873
gumraze-git <galaxydh4110@gmail.com>
2026-02-08 22:54:00 +0900

    [REFAC] RestTemplate 설정 리팩토링 및 Properties 구성 도입
    - RestTemplate 설정 클래스를 RestClient 기반으로 리팩토링
    - FastApiProperties 및 KobisApiProperties 클래스 추가
    - application-local.yml 파일에 fastapi, kobis 프로퍼티 구성 변경

---
+++ src/main/java/com/insidemovie/backend/common/config/KobisApiProperties.java
@@ -0,0 +1,14 @@
+package com.insidemovie.backend.common.config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "kobis.api")
+public class KobisApiProperties {
+    private String baseUrl;
+}
